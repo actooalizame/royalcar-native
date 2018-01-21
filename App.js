3155a3eb15c71880fa8b39a94013bfc0dcd9b1fd
actooalizame <gndalmedo@gmail.com>
2018-01-21 06:58:55 -0300

    added kitten-ui

--- App.js
+++ App.js
@@ -12,6 +12,8 @@ import {
   View
 } from 'react-native';
 
+import {RkButton} from 'react-native-ui-kitten';
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
@@ -32,6 +34,7 @@ export default class App extends Component<{}> {
         <Text style={styles.instructions}>
           {instructions}
         </Text>
+        <RkButton>Click me!</RkButton>
       </View>
     );
   }
